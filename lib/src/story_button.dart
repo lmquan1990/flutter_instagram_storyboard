ec32aaece936885c8de4c6d31a484fa84113bd50
lmquan1990 <lmquan1990@gmail.com>
2024-10-02 16:18:40 +0000

    nt

--- lib/src/story_button.dart
+++ lib/src/story_button.dart
@@ -137,43 +137,6 @@ class _StoryButtonState extends State<StoryButton>
               child: Stack(
                 children: [
                   widget.buttonData.buttonContainer,
-                  // Container(
-                  //   width: double.infinity,
-                  //   height: double.infinity,
-                  //   decoration: widget.buttonData.buttonDecoration,
-                  // ),
-                  // Container(
-                  //   height: 150,
-                  //   width: 150,
-                  //   padding: const EdgeInsets.only(top: 10, left: 10),
-                  //   decoration: BoxDecoration(
-                  //     color: Colors.teal,
-                  //     borderRadius: BorderRadius.circular(15.0),
-                  //   ),
-                  //   child: Stack(
-                  //     children: const [
-                  //       Align(
-                  //         alignment: Alignment.topLeft,
-                  //         child: Text(
-                  //           "History",
-                  //           style: TextStyle(
-                  //             color: Colors.white,
-                  //             fontSize: 25,
-                  //           ),
-                  //         ),
-                  //       ),
-                  //       Positioned(
-                  //         right: -25,
-                  //         bottom: -25,
-                  //         child: Icon(
-                  //           Icons.history,
-                  //           color: Colors.cyan,
-                  //           size: 120,
-                  //         ),
-                  //       ),
-                  //     ],
-                  //   ),
-                  // ),
                   Material(
                     color: Colors.transparent,
                     child: InkWell(
@@ -317,12 +280,6 @@ class StoryButtonData {
     this.containerBackgroundDecoration = const BoxDecoration(
       color: Color.fromARGB(255, 0, 0, 0),
     ),
-    // this.buttonDecoration = const BoxDecoration(
-    //   borderRadius: BorderRadius.all(
-    //     Radius.circular(15.0),
-    //   ),
-    //   color: Color.fromARGB(255, 226, 226, 226),
-    // ),
     required this.buttonContainer,
     this.borderDecoration = const BoxDecoration(
       borderRadius: BorderRadius.all(
